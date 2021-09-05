bb2ee0bacd3c1dad22b0e957c6e8dafe6f87a303
susanelizabetth <castillosusana55@gmail.com>
2021-09-04 19:36:48 -0500

    set up the base template, Create django app and models from product

--- django_deepeach/django_deepeach/urls.py
+++ django_deepeach/django_deepeach/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include, include
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
      path('api/v/1', include('djoser.urls.authtoken')),
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
