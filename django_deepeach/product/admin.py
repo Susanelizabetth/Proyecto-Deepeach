fbd0cd37dc4e8fbf242fd7a6c4ff5306f38f128d
susanelizabetth <castillosusana55@gmail.com>
2021-09-05 23:00:52 -0500

    added product to admin portal

--- django_deepeach/product/admin.py
+++ django_deepeach/product/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+
+from .models import Product
+
+
+@admin.register(Product)
+class PartnersAdmin(admin.ModelAdmin):
+    
+    list_display = ('name','category','price','date_added')
+    #list_display_links = ('code','identity')
+    verbose_name = "Products"
+    list_filter = ('category',)
+    search_fields = ('name','category','price','date_added')
+    list_editable = ('category','price')
+    #readonly_fields = ('entity_type','code_type')
