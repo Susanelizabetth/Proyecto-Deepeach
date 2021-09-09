b66fb9803cdf645aae980794f6a70aabae6d7f6c
susanelizabetth <castillosusana55@gmail.com>
2021-09-08 20:30:53 -0500

    (0:57:27) Make it possible to add to cart

--- frontend_vue/src/store/index.js
+++ frontend_vue/src/store/index.js
@@ -7,9 +7,27 @@ export default createStore({
     },
     isAutenticated: false,
     token: '',
-    isLoading: false
+    isLoading: false   
   },
   mutations: {
+    initializeStore(state){
+      if (localStorage.getItem('cart') != "undefined"){
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else{
+          localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item){
+      const exist = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      if (exist.length){
+        exist[0].quantity = parseInt(exist[0].quantity)+parseInt(item.quantity)
+      } else{
+        state.cart.items.push(item)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
--- frontend_vue/src/views/Product.vue
+++ frontend_vue/src/views/Product.vue
@@ -16,7 +16,7 @@
                         <input type="number" class="input" min="1" v-model="quantity">
                     </div>
                     <div class="control">
-                        <a href="" class="button is-dark">Add to cart</a>
+                        <a class="button is-dark" @click="addToCart">Add to cart</a>
                     </div>
 
                 </div>
@@ -28,6 +28,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'Product',
@@ -53,6 +54,27 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+        },
+        addToCart(){
+            if (isNaN(this.quantity) || this.quantity < 1) {
+                this.quantity = 1
+            }
+
+            const item = {
+                product: this.product,
+                quantity: this.quantity
+            }
+
+            this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'Se ha agergado el producto al carrito',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            })
         }
     }
 
