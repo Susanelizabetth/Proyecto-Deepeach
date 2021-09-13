09cb728a70508be67031b79f92d5d76e7fcdb89a
susanelizabetth <castillosusana55@gmail.com>
2021-09-12 23:21:43 -0500

    (1:05:31) Implement a loading bar

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -25,12 +25,14 @@
           </div>
         </div>
     </nav>
-
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading}">
+        <div class="lds-dual-ring"></div>
+    </div>
     <section class="section">
      <router-view/>
     </section>
     <footer class="footer">
-      <p class="has-text-cerntered">Copyright (c) 2021</p>
+      <p class="has-text-centered">Copyright (c) 2021</p>
     </footer>
   </div>
 </template>
@@ -67,4 +69,41 @@ export default {
 
 <style lang="scss">
 @import '../node_modules/bulma';
+.lds-dual-ring{
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after{
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 8px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color:#ccc transparent #ccc transparent;
+  
+}
+
+@keyframes lds-dual-ring {
+  0%{
+    transform: rotate(0deg);
+  }
+
+  100%{
+    transform: rotate(360deg);
+  }
+  
+}
+.is-loading-bar{
+  height: 0;
+  overflow: hidden;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &.is-loading{
+    height: 80px;
+  }
+}
 </style>
