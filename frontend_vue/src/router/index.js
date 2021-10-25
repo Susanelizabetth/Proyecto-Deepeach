9496d67056e19c940d00bd400647f5e704d39502
Eivar Morales <eivaremir@gmail.com>
2021-10-25 18:08:29 -0500

    2:00:00 create simple success page

--- frontend_vue/src/router/index.js
+++ frontend_vue/src/router/index.js
@@ -9,7 +9,7 @@ import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import MyAccount from '../views/MyAccount.vue'
 import Checkout from '../views/Checkout.vue'
-
+import Success from '../views/Success.vue'
 
 
 const routes = [
@@ -48,6 +48,11 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/cart/success',
+    name: 'Success',
+    component: Success
   },
   {
     path: '/search',
